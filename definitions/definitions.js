b99719c798651e8f66c3e9038f11766fc45cc49d
BorakL <lborak88@gmail.com>
2024-08-29 02:33:31 +0200

    added updateTeamStats and addMatch functions

--- definitions/definitions.js
+++ definitions/definitions.js
@@ -3,3 +3,18 @@ export const groupStageMatches = [
     [[1,3], [2,4]],
     [[1,2], [3,4]]
 ]
+
+export const teamInitStatistics = {
+    wins:0,
+    loses:0,
+    pointsScored:0,
+    pointsConceded:0,
+    pointDifference:0
+}
+
+export const stagesDefinitions = {
+    group: "group",
+    quarterfinals: "quarterfinals",
+    semifinals: "semifinals",
+    finals: "finals"
+}
--- index.js
+++ index.js
@@ -1,18 +1,118 @@
 import groups from "./data/groups.json" assert { type: "json" };
-import { groupStageMatches } from "./definitions/definitions.js";
+import { groupStageMatches, stagesDefinitions, teamInitStatistics, tournamentStages } from "./definitions/definitions.js";
 
 // console.log(groups)
   
 
 const groupsKeys = Object.keys(groups)
-const teams = [];
+console.log(groupsKeys)
+
+const teams = {};
 groupsKeys.forEach(group => groups[group].forEach(team => 
-    teams.push({...team})
-) )
+    teams[team.ISOCode] = {...team, group}
+))
 
-const groupStageMatchesData = [];
+const tournament = {
+    tournament: "Olympic Games Basketball 2024",
+    startDate: "2024-07-05",
+    endDate: "2024-08-08",
+    totalTeam: 12,
+    winner: "",
+    stages: {
+        groupStage: {
+            "A": [],
+            "B": [],
+            "C": []
+        }, 
+        quarterFinals: [],
+        semiFinals: [],
+        finals: [] 
+    }, 
+    teams
+} 
 
-const match = (team1,team2) => {
+
+
+const matchesData = [];
+
+const updateTeamStats = (tournament, team, update, stageInfo) => {
+    if(!Object.values(tournamentStages).includes(stageInfo.stage)){
+        throw new Error("Invalid stage!")
+    }
+    if(stageInfo.stage === "group"){
+        if(!groupsKeys.includes(stageInfo.group) || ![1,2,3].includes(stageInfo.phase)){
+            throw new Error("Invalid group stage info!")
+        }
+    }
+    
+    // let overalStats = tournament.teams[team]?.overalStats; 
+    // if(!overalStats){
+    //     overalStats = {...teamInitStatistics}
+    // }
+
+
+    let groupStageStats = tournament.teams[team]?.groupStageStats 
+                            ? {...tournament.teams[team].groupStageStats}
+                            : {...teamInitStatistics}
+
+    switch(stageInfo.stage){
+        case stagesDefinitions.group: {
+            for(let key in update){
+                if(groupStageStats.hasOwnProperty(key)){
+                    groupStageStats[key] += update[key]
+                }
+            }
+            tournament.teams = tournament.teams || {};
+            tournament.teams[team] = tournament.teams[team] || {};
+            tournament.teams[team].groupStageStats = groupStageStats;
+            //overallStats
+            break;
+        }
+        
+    } 
+
+}
+
+
+const addMatch = (tournament, match, stageInfo) => {
+    const { group, quarterfinals, semifinals, finals } = stagesDefinitions;
+    if(!Object.values(tournamentStages).includes(stageInfo.stage)){
+        throw new Error("Invalid stage!")
+    }
+    if(stageInfo.stage === group){
+        if(!groupsKeys.includes(stageInfo.group) || ![1,2,3].includes(stageInfo.round)){
+            throw new Error("Invalid group stage info!")
+        }
+    }
+    tournament.stages = tournament.stages || {};
+     
+    switch(stageInfo.stage){
+        case group: {
+            tournament.stages[group] = tournament.stages.groupStage || {};
+            tournament.stages[group][stageInfo.group] = tournament.stages.groupStage[stageInfo.group] || [];
+            tournament.stages[group][stageInfo.group].push(match);
+            break;
+        };
+        case quarterfinals: {
+            tournament.stages[quarterfinals] = tournament.stages[quarterfinals] || [];
+            tournament.stages[quarterfinals].push(match);
+            break;
+        };
+        case semifinals: {
+            tournament.stages[semifinals] = tournament.stages[semifinals] || [];
+            tournament.stages[semifinals].push(match);
+            break;
+        }
+        case finals: {
+            tournament.stages[finals] = tournament.stages[finals] || [];
+            tournament.stages[finals].push(match);
+            break;
+        }
+    }
+}
+
+
+const match = (team1, team2, tournament, stageInfo) => {
     console.log("team1", team1)
     console.log("team2", team2)
     const max = 80;
@@ -20,43 +120,67 @@ const match = (team1,team2) => {
 
     let disqualification = Math.ceil(Math.random()*100)
     let t1Points, t2Points;
-    let t1BasketScored, t2BasketScored;
+    let t1PointsScored, t2PointsScored;
     if(disqualification===1){
         t1Points = 0;
         t2Points = 2;
-        t1BasketScored = 0;
-        t2BasketScored = 20;
+        t1PointsScored = 0;
+        t2PointsScored = 20;
     }else if(disqualification===2){
         t1Points = 2;
         t2Points = 0;
-        t1BasketScored = 20;
-        t2BasketScored = 0;
+        t1PointsScored = 20;
+        t2PointsScored = 0;
     }else{
         //Pazi ovde da ne može da se nađe isti rezultat
-        t1BasketScored = Math.ceil(Math.random()*(max-min)+min)
-        t2BasketScored = Math.ceil(Math.random()*(max-min)+min)
-        t1Points = t1BasketScored > t2BasketScored ? 2 : 1;
-        t2Points = t1BasketScored < t2BasketScored ? 2 : 1
+        t1PointsScored = Math.ceil(Math.random()*(max-min)+min)
+        t2PointsScored = Math.ceil(Math.random()*(max-min)+min)
+        t1Points = t1PointsScored > t2PointsScored ? 2 : 1;
+        t2Points = t1PointsScored < t2PointsScored ? 2 : 1
+    }
+ 
+    let team1Update = {
+        wins: t1Points>t2Points ? 1 : 0,
+        loses: t1Points<t2Points ? 1 : 0,
+        pointsScored: t1PointsScored,
+        pointsConceded: t2PointsScored,
+        pointDifference: t1PointsScored-t2PointsScored
+    }
+    let team2Update = {
+        wins: t2Points>t1Points ? 1 : 0,
+        loses: t2Points<t1Points ? 1 : 0,
+        pointsScored: t2PointsScored,
+        pointsConceded: t1PointsScored,
+        pointDifference: t2PointsScored-t1PointsScored
     }
 
-    return ({ 
-        team1: {...team1, basketScored: t1BasketScored, points:t1Points},
-        team2: {...team2, basketScored: t2BasketScored, points:t2Points}
-    })
+    updateTeamStats(tournament, team1.ISOCode, team1Update, stageInfo)
+    updateTeamStats(tournament, team2.ISOCode, team2Update, stageInfo)
 
+    let matchData = {
+        [team1.ISOCode]:{
+            team: team1.Team,
+            score: t1PointsScored
+        },
+        [team2.ISOCode]:{
+            team: team2.Team,
+            score: t2PointsScored
+        },
+        stageInfo
+    }
+    addMatch(tournament, matchData, stageInfo)
 }
 
 
-
-groupStageMatches.forEach((round,i) => {
-    console.log(`round: ${i+1} ///////////////////`)
-    round.forEach((positions) => {
-        groupsKeys.forEach(g => 
-        {
-            groupStageMatchesData.push( { ...match(groups[g][positions[0]-1], groups[g][positions[1]-1]), group: g, round:i+1}  )
-        })
-    })
-} )
+// groupStageMatches.forEach((round,i) => {
+//     console.log(`round: ${i+1} ///////////////////`)
+//     round.forEach((positions) => {
+//         groups.forEach(g => 
+//         {
+//             matchesData.push( { ...match(groups[g][positions[0]-1], groups[g][positions[1]-1]), group: g, round:i+1}  )
+//         })
+//     })
+// } )
 
 
 // const groupStageStatistics = groupStageMatchesData.reduce((a,b)=>{
@@ -64,4 +188,5 @@ groupStageMatches.forEach((round,i) => {
 // })
 
 
-console.log("groupStageMatches",groupStageMatchesData)
+
+// console.log("groupStageMatches", matchesData)
